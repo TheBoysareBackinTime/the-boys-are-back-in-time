240282a4d07065801a168d7889c74dad1fbfb07c
Dave Powers <djpowers89@gmail.com>
2020-03-31 23:50:21 -0400

    Add source plugin to load podcast feed metadata
    - using placeholder RSS feed URL for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-podcast-rss-feed`,
+      options: {
+        feedURL: `https://some.url/yourpodcastfeed.rss`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
