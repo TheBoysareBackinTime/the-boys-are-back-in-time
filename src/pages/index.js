fd4c26ac744eb3091783087488ac2e801632c9a4
Dave Powers <djpowers89@gmail.com>
2020-05-01 15:32:59 -0400

    Display episodes on index page
    - add GraphQL query and import
    - loop through and display basic attributes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="The Boys Are Back In Time Podcast" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    {data.allPodcastRssFeedEpisode.edges.map(({ node }, index) => (
+      <div key={index}>
+        <p className="episode-date">{node.item.pubDate}</p>
+        <p className="episode-title">{node.item.title}</p>
+        <p className="episode-summary">{node.item.itunes.summary}</p>
+      </div>
+    ))}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
@@ -19,3 +26,26 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    allPodcastRssFeedEpisode {
+      edges {
+        node {
+          id
+          item {
+            guid
+            itunes {
+              author
+              duration
+              image
+              summary
+            }
+            pubDate
+            title
+          }
+        }
+      }
+    }
+  }
+`
