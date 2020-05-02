31a0219b40fdd232b57f5e211b5946e1db04caf5
Dave Powers <djpowers89@gmail.com>
2020-05-02 16:26:30 -0400

    Fill in data for episode template page
    - query data using slug
    - set HTML using encoded content

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -1,10 +1,32 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => {
+export default ({ data }) => {
+  const episode = data.podcastRssFeedEpisode
   return (
     <Layout>
-      <div>Hello episode</div>
+      <div>
+        <h1>{episode.item.title}</h1>
+        <div
+          dangerouslySetInnerHTML={{ __html: episode.item.content_encoded }}
+        />
+      </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query MyQuery($slug: String!) {
+    podcastRssFeedEpisode(fields: { slug: { eq: $slug } }) {
+      id
+      fields {
+        slug
+      }
+      item {
+        title
+        content_encoded
+      }
+    }
+  }
+`
