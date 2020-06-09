4616478ff523f6b955b5a03abb42abb61743fdd2
Dave Powers <djpowers89@gmail.com>
2020-06-09 17:30:44 -0400

    Add audio player to episode pages
    - use React H5 Audio Player
    - make episode URL available to GraphQL query
    - set progress jump steps

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -2,6 +2,8 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
+import AudioPlayer from "react-h5-audio-player"
+import "react-h5-audio-player/lib/styles.css"
 
 export default ({ data }) => {
   const episode = data.podcastRssFeedEpisode
@@ -10,6 +12,12 @@ export default ({ data }) => {
       <div>
         <h1>{episode.item.title}</h1>
         <Img fixed={episode.featuredImg.childImageSharp.fixed} />
+        <AudioPlayer
+          autoPlay
+          src={episode.item.enclosure.url}
+          onPlay={(e) => console.log("onPlay")}
+          progressJumpSteps={{ backward: 15000, forward: 15000 }}
+        />
         <div
           dangerouslySetInnerHTML={{ __html: episode.item.content_encoded }}
         />
@@ -28,6 +36,9 @@ export const query = graphql`
       item {
         title
         content_encoded
+        enclosure {
+          url
+        }
       }
       featuredImg {
         childImageSharp {
