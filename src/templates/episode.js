f194b5e24729955d27a7b3000ecd71d0ada78261
Dave Powers <djpowers89@gmail.com>
2020-06-09 17:56:49 -0400

    Add custom icons for skip forward/back
    - set different jump steps for each direction
    - add attribution for icons

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -4,6 +4,8 @@ import Layout from "../components/layout"
 import Img from "gatsby-image"
 import AudioPlayer from "react-h5-audio-player"
 import "react-h5-audio-player/lib/styles.css"
+import Back15Button from "../images/iconfinder_btn_15_Forward_4719305.svg"
+import Forward30Button from "../images/iconfinder_btn_30_Backward_4719314.svg"
 
 export default ({ data }) => {
   const episode = data.podcastRssFeedEpisode
@@ -16,11 +18,35 @@ export default ({ data }) => {
           autoPlay
           src={episode.item.enclosure.url}
           onPlay={(e) => console.log("onPlay")}
-          progressJumpSteps={{ backward: 15000, forward: 15000 }}
+          progressJumpSteps={{ backward: 15000, forward: 30000 }}
+          customIcons={{
+            rewind: <img src={Back15Button} alt="logo" width="100%"></img>,
+            forward: <img src={Forward30Button} alt="logo" width="100%"></img>,
+          }}
         />
         <div
           dangerouslySetInnerHTML={{ __html: episode.item.content_encoded }}
         />
+        <span>
+          Skip forward/back icons by{" "}
+          <a
+            href="https://www.iconfinder.com/ramankumar"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Raman Kumar
+          </a>{" "}
+          under{" "}
+          <a
+            href="https://creativecommons.org/licenses/by/3.0/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Creative Commons
+          </a>{" "}
+          license.
+        </span>
       </div>
     </Layout>
   )
