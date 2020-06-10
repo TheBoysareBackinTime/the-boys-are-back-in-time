0e517d6cd0604800dfdf7490566da817768b251d
Dave Powers <djpowers89@gmail.com>
2020-06-10 16:40:15 -0400

    Add placeholder subscribe badges to landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,45 @@ import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import AppleBadge from "../images/badge-apple-podcasts.svg"
+import OvercastBadge from "../images/badge-overcast.svg"
+import PocketCastsBadge from "../images/badge-pocket-casts.svg"
+import RadioPublicBadge from "../images/badge-radiopublic.svg"
+
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="The Boys Are Back In Time Podcast" />
-    <h1>Episodes</h1>
+    <h2 style={{ "text-align": `center` }}>Subscribe in your favorite app</h2>
+    <div
+      className="subscribeBadges"
+      style={{
+        display: `flex`,
+        "flex-wrap": `wrap`,
+        "justify-content": `center`,
+        "margin-bottom": `1rem`,
+      }}
+    >
+      <a href="">
+        <img src={AppleBadge} style={{ height: `50px`, margin: `.25rem` }} />
+      </a>
+      <a href="">
+        <img src={OvercastBadge} style={{ height: `50px`, margin: `.25rem` }} />
+      </a>
+      <a href="">
+        <img
+          src={PocketCastsBadge}
+          style={{ height: `50px`, margin: `.25rem` }}
+        />
+      </a>
+      <a href="">
+        <img
+          src={RadioPublicBadge}
+          style={{ height: `50px`, margin: `.25rem` }}
+        />
+      </a>
+    </div>
+
+    <h2>Episodes</h2>
     {data.allPodcastRssFeedEpisode.edges.map(({ node }, index) => (
       <div key={index}>
         <p className="episode-date">{node.item.pubDate}</p>
