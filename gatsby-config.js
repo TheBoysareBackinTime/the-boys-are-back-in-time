39a1dd637a0ad808bf0c70cc4ffdb7facdbba031
Dave Powers <djpowers89@gmail.com>
2020-10-24 23:02:51 -0400

    Configure remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,27 @@ module.exports = {
         source: (node) => node.item.title,
       },
     },
-    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    }`gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
