f0d9340941afc2b3924a35500c0f769299ffd6cd
Dave Powers <djpowers89@gmail.com>
2020-12-06 21:50:29 -0500

    Add post descriptions to blogs index page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,10 +5,18 @@ import Layout from "../components/layout"
 export default ({ data }) => {
   return (
     <Layout>
+      <h2 style={{ textAlign: `center` }}>Blog Posts</h2>
       {data.allMarkdownRemark.edges.map(({ node }, index) => (
-        <Link key={index} to={`/blog${node.fields.slug}`} className="blog-post">
-          {node.frontmatter.title}
-        </Link>
+        <>
+          <Link
+            key={index}
+            to={`/blog${node.fields.slug}`}
+            className="blog-post"
+          >
+            {node.frontmatter.title}
+          </Link>
+          <p className="blog-description">{node.frontmatter.description}</p>
+        </>
       ))}
     </Layout>
   )
